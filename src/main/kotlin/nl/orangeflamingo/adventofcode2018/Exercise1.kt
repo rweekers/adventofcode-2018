7eeee3e9de4a7c557096e82887205dc442018f36
Remco Weekers <remco@craftsmen.nl>
2018-12-01 10:21:06 +0100

    Solutions to exercise 1 silver and gold

--- src/main/kotlin/nl/orangeflamingo/adventofcode2018/Exercise1.kt
+++ src/main/kotlin/nl/orangeflamingo/adventofcode2018/Exercise1.kt
@@ -1,11 +1,27 @@
 package nl.orangeflamingo.adventofcode2018
 
-class Exercise1(private val input: String) {
+class Exercise1(input: String) {
 
     private val inputList: List<String> = resourceAsListOfString(input)
 
     fun silverExercise(): Int {
-        inputList.forEach { println(it) }
+        return inputList.map { it.toInt() }.reduce { acc, curr -> acc + curr }
+    }
+
+    fun goldExercise(): Int {
+        var currentFreq = 0
+        var frequencies = mutableSetOf<Int>()
+        frequencies.add(currentFreq)
+        while (true) {
+            inputList.map { it.toInt() }
+                    .forEach {
+                        currentFreq += it
+                        if (frequencies.contains(currentFreq)) {
+                            return currentFreq
+                        }
+                        frequencies.add(currentFreq)
+                    }
+        }
         return 0
     }
 
@@ -14,5 +30,6 @@ class Exercise1(private val input: String) {
 
 fun main(args: Array<String>) {
     val exc1 = Exercise1("/input1.txt")
-    exc1.silverExercise()
+    println("The answer to the silver exercise is ${exc1.silverExercise()}")
+    println("The answer to the gold exercise is ${exc1.goldExercise()}")
 }
--- src/test/kotlin/nl/orangeflamingo/adventofcode2018/Exercise1Test.kt
+++ src/test/kotlin/nl/orangeflamingo/adventofcode2018/Exercise1Test.kt
@@ -13,19 +13,49 @@ class Exercise1Test {
 
     @Test
     fun `exercise 1-2 silver test`() {
-        val exc1 = Exercise1("/one/test1-2.txt")
+        val exc1 = Exercise1("/one/test1-2-silver.txt")
         assertThat(exc1.silverExercise()).isEqualTo(3)
     }
 
     @Test
     fun `exercise 1-3 silver test`() {
-        val exc1 = Exercise1("/one/test1-3.txt")
+        val exc1 = Exercise1("/one/test1-3-silver.txt")
         assertThat(exc1.silverExercise()).isEqualTo(0)
     }
 
     @Test
     fun `exercise 1-4 silver test`() {
-        val exc1 = Exercise1("/one/test1-4.txt")
+        val exc1 = Exercise1("/one/test1-4-silver.txt")
         assertThat(exc1.silverExercise()).isEqualTo(-6)
+    }
+
+    @Test
+    fun `exercise 1-1 gold test`() {
+        val exc1 = Exercise1("/one/test1-1.txt")
+        assertThat(exc1.goldExercise()).isEqualTo(2)
+    }
+
+    @Test
+    fun `exercise 1-2 gold test`() {
+        val exc1 = Exercise1("/one/test1-2-gold.txt")
+        assertThat(exc1.goldExercise()).isEqualTo(0)
+    }
+
+    @Test
+    fun `exercise 1-3 gold test`() {
+        val exc1 = Exercise1("/one/test1-3-gold.txt")
+        assertThat(exc1.goldExercise()).isEqualTo(10)
+    }
+
+    @Test
+    fun `exercise 1-4 gold test`() {
+        val exc1 = Exercise1("/one/test1-4-gold.txt")
+        assertThat(exc1.goldExercise()).isEqualTo(5)
+    }
+
+    @Test
+    fun `exercise 1-5 gold test`() {
+        val exc1 = Exercise1("/one/test1-5-gold.txt")
+        assertThat(exc1.goldExercise()).isEqualTo(14)
     }
 }
