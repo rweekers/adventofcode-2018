ff33a393efd95c85bff726cfd14d717927126445
Remco Weekers <remco@craftsmen.nl>
2018-12-09 15:10:15 +0100

    Exercise 9 gold solution

--- src/main/kotlin/nl/orangeflamingo/adventofcode2018/Exercise9.kt
+++ src/main/kotlin/nl/orangeflamingo/adventofcode2018/Exercise9.kt
@@ -1,66 +1,39 @@
 package nl.orangeflamingo.adventofcode2018
 
+import java.util.*
+
 class Exercise9(private val circularList: CircularList = CircularList()) {
 
-    fun exercise(highestMarble: Int = 25, numberOfPlayers: Int = 9, scoresPerPlayer: MutableMap<Int, Long> = mutableMapOf()): Long {
-        (1..highestMarble).forEach {
-            val player = if (it % numberOfPlayers == 0) numberOfPlayers else it % numberOfPlayers
+    fun exercise(highestMarble: Int = 25, numberOfPlayers: Int = 9): Long {
+        val scores = LongArray(numberOfPlayers)
 
+        (1..highestMarble).forEach {
+            val player = it % numberOfPlayers
             val score = circularList.addMarble(it)
 
-            val oldScore = scoresPerPlayer.getOrDefault(player, 0)
-
-            scoresPerPlayer[player] = oldScore + score
+            scores[player] += score
         }
-
-        val playerWithHighestScore = scoresPerPlayer.maxBy { it.value }
-
-        return playerWithHighestScore!!.value
+        return scores.max()!!
     }
 
-    fun goldExercise(): Int {
-        return 0
-    }
-
-    data class CircularList(val theList: MutableList<Int> = mutableListOf(0)) {
-
-        private var currentIndex = 0
-
-        fun addMarble(marbleValue: Int): Int {
-            if (marbleValue % 23 == 0) {
-                val indexMarbleToTake = getCircularIndex(currentIndex - 7)
-                currentIndex = indexMarbleToTake
-                val marbleToTake = theList[indexMarbleToTake]
-                theList.removeAt(indexMarbleToTake)
-                return marbleToTake + marbleValue
-            }
-
-            when {
-                theList.size < 2 -> theList.add(marbleValue)
-                theList.size == 2 -> {
-                    currentIndex = 1
-                    theList.add(currentIndex, marbleValue)
-                }
-                else -> {
-                    // get index to insert
-                    currentIndex += 2
-
-                    currentIndex = getCircularIndex(currentIndex)
-
-                    theList.add(currentIndex, marbleValue)
-                }
-            }
-            return 0
+    data class CircularList(val theList: ArrayDeque<Int> = ArrayDeque()) {
+        init {
+            theList.addFirst(0)
         }
 
-        private fun getCircularIndex(newIndex: Int): Int {
-            if (newIndex >= 0) {
-                if (newIndex >= theList.size) {
-                    return newIndex % theList.size
+        fun addMarble(marbleValue: Int): Long {
+            return if (marbleValue % 23 == 0) {
+                (0 until 7 - 1).forEach { _ ->
+                    theList.addLast(theList.remove())
                 }
-                return newIndex
+                theList.pop().toLong() + marbleValue
             } else {
-                return theList.size + newIndex
+                // move two ahead
+                (0 until 2).forEach { _ ->
+                    theList.addFirst(theList.removeLast())
+                }
+                theList.addLast(marbleValue)
+                0
             }
         }
     }
